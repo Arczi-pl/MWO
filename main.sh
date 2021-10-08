b56d197d988e6876d29a9f8968f6e27fa8e515a6
Arczi-pl <artursikorski4@gmail.com>
2021-10-08 12:15:58 +0200

    frontend hotreload

--- main.sh
+++ main.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 build() {
-    echo "Start building"
     docker-compose build
     
 }
 
 up() {
-    echo "Start uping"
     docker-compose up -d
 }
 
 follow() {
-    echo "Start following"
     docker-compose logs --follow --tail 50
 }
 
 down() {
-    echo "Kill all containers"
     docker-compose down
 }
 
