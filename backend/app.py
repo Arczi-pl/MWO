e216d8447a9421620e7c5b93a9506a313193abef
Arczi-pl <artursikorski4@gmail.com>
2021-10-08 11:48:32 +0200

    flask hotreload

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask 
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return 'Hello API!'
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0')
